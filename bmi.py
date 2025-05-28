6e6a0d220624cb8729d510d2decea2687ec1a399
Ei Than Thar Aung <thanthar.24@ichat.sp.edu.sg>
2025-05-28 12:51:01 +0800

    modification for lab3

--- bmi.py
+++ bmi.py
@@ -4,11 +4,11 @@ def calculate_bmi(height,weight):
     bmi=weight/(height*height)
     print(f"BMI value is {bmi:2f}")
     if(bmi<18.5):
-        print("Under Weight")
+        return -1
     elif (18.5<=bmi<=25.0):
-        print("Normal Weight")
+        return 0
     elif(bmi>25.0):
-        print("Over Weight")
+        return 1
 if name=="_main_":
     main()
 calculate_bmi(weight=57.90,height=1.63)
